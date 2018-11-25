8ea90e86877bb04b2f9c138bf863439231d10c45
krishna21august <krishnakant.singh.min12@gmail.com>
2018-11-25 18:24:57 +0530

    add enzyme for testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "../components/App";
+import CommentBox from "../components/CommentBox";
 
-it("renders without crashing", () => {
+it("shows a coment box", () => {
   const div = document.createElement("div");
+
   ReactDOM.render(<App />, div);
-  expect(div.innerHTML).toContain("Hello");
   ReactDOM.unmountComponentAtNode(div);
 });
