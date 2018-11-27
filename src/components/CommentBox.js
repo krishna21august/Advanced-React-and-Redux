e60ee9770835f9a439d7623d152ad06efff5c02b
safe <krishna.s@lucideustech.com>
2018-11-27 18:26:16 +0530

    first form with state set

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,28 @@
-import React from "react";
-export default () => {
-  return <div>Comment Box</div>;
-};
+import React, { Component } from "react";
+
+class CommentBox extends Component {
+  state = { comment: "" };
+
+  /*Binds the event to a target and gets the value to state */
+  handleChange = event => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({ comment: "" });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a comment</h4>
+        <textarea onChange={this.handleChange} value={this.state.comment} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
