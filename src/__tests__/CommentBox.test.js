7e81d0de76993e148f18c426043fde5fe3d8fc43
krishna21august <krishnakant.singh.min12@gmail.com>
2018-12-02 13:47:23 +0530

    simulate for submit

--- src/__tests__/CommentBox.test.js
+++ src/__tests__/CommentBox.test.js
@@ -20,4 +20,17 @@ it("has text area that users can type in", () => {
   wrapped.find("textarea").simulate("change", {
     target: { value: "new comment" }
   });
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("when form is submitted , text area gets emptied", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" }
+  });
+  wrapped.update();
+
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value").length).toEqual(0);
 });
